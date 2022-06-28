9f322df14dc378143c464856ab816730df3661c0
Kelsi Lane <kelsilane91@gmail.com>
2022-06-28 13:02:57 -0500

    first cypress test

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,5 +1,15 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("Navigation", () => {
+  it("should navigate a blog page", () => {
+    // Start from the index page
+    cy.visit("http://localhost:3000");
+
+    // Find a link with an href attribute containing "about" and click it
+    cy.get('a[href*="posts/ssg-ssr"]').click();
+
+    // The new url should include "/about"
+    cy.url().should("include", "/posts/ssg-ssr");
+
+    // The new page should contain an h1 with "About page"
+    cy.get("h1").contains("When to Use Static Generation");
+  });
+});
